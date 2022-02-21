41b4171f6561586e2b60735c35c96cbd2a95ef58
lam6670 <55042764+lam6670@users.noreply.github.com>
2022-02-22 03:01:29 +0700

    Second Commit

--- BatStore/BatStore/Areas/Admin/Controllers/AdminProductsController.cs
+++ BatStore/BatStore/Areas/Admin/Controllers/AdminProductsController.cs
@@ -21,16 +21,39 @@ namespace BatStore.Areas.Admin.Controllers
         }
 
         // GET: Admin/AdminProducts
-        public IActionResult Index(int? page)
+        public IActionResult Index(int page=1, int CatID =0)
         {
-            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageNumber = page;
             var pageSize = 20;
-            var lsProducts = _context.Products.AsNoTracking().Include(x=>x.Cat).OrderByDescending(x => x.ProductId);
-            PagedList<Product> models = new PagedList<Product>(lsProducts, pageNumber, pageSize);
+            List<Product> lsProducts = new List<Product>();
+            if(CatID !=0)
+            {
+                lsProducts = _context.Products
+                    .AsNoTracking()
+                    .Where(x=>x.CatId==CatID)
+                    .Include(x => x.Cat)
+                    .OrderByDescending(x => x.ProductId)
+                    .ToList();
+            }
+            else
+            {
+                lsProducts = _context.Products.AsNoTracking().Include(x => x.Cat).OrderByDescending(x => x.ProductId).ToList();
+            }
+            PagedList<Product> models = new PagedList<Product>(lsProducts.AsQueryable(), pageNumber, pageSize);
+            ViewBag.CurrentCateID = CatID;
             ViewBag.CurrentPage = pageNumber;
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", CatID);
             return View(models);
         }
-
+        public IActionResult Filtter(int CatID = 0)
+        {
+            var url = $"/Admin/AdminProducts?CatID={CatID}";
+            if (CatID == 0)
+            {
+                url = $"/Admin/AdminProducts";
+            }
+            return Json(new { status = "success", redirectUrl = url });
+        }
         // GET: Admin/AdminProducts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -53,7 +76,7 @@ namespace BatStore.Areas.Admin.Controllers
         // GET: Admin/AdminProducts/Create
         public IActionResult Create()
         {
-            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId");
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName");
             return View();
         }
 
@@ -70,7 +93,7 @@ namespace BatStore.Areas.Admin.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId", product.CatId);
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", product.CatId);
             return View(product);
         }
 
@@ -87,7 +110,7 @@ namespace BatStore.Areas.Admin.Controllers
             {
                 return NotFound();
             }
-            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId", product.CatId);
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", product.CatId);
             return View(product);
         }
 
@@ -123,7 +146,7 @@ namespace BatStore.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId", product.CatId);
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", product.CatId);
             return View(product);
         }
 
---
+++ BatStore/BatStore/Extention/Extention.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BatStore.Extention
+{
+    public static class Extention
+    {
+        public static string ToVND(this double donGia)
+        {
+            return donGia.ToString("#,##0") + " VND";
+        }
+        public static string ToTitleCase(string str)
+        {
+            string result = str;
+            if(!string.IsNullOrEmpty(str))
+            {
+                var words = str.Split(' ');
+                for(int i = 0; i <words.Length; i++)
+                {
+                    var s = words[i];
+                    if(s.Length>0)
+                    {
+                        words[i] = s[0].ToString().ToUpper() + s.Substring(1);
+                    }
+                }
+                result = string.Join(" ", words);
+            }
+            return result;
+        }
+        public static string ToEasyUrl(this string url)
+        {
+            var result = url.ToLower().Trim();
+            result = Regex.Replace(result, "aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬ", "a");
+            result = Regex.Replace(result, "dDđĐ", "d");
+            result = Regex.Replace(result, "eEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆ", "e");
+            result = Regex.Replace(result, "iIìÌỉỈĩĨíÍịỊ", "i");
+            result = Regex.Replace(result, "oOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢ", "o");
+            result = Regex.Replace(result, " UùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰ", "u");
+            result = Regex.Replace(result, "YỳỲỷỶỹỸýÝỵỴ", "y");
+            result = Regex.Replace(result, "[^a-z0-9]", " ");
+            result = Regex.Replace(result, "(-)+", "-");
+            return result;
+        }
+    }
+}
